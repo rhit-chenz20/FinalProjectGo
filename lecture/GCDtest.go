e29612c3ee33efe51f70d576b6f3d7f3fc6afe3e
rhit-pascenaj <73701185+rhit-pascenaj@users.noreply.github.com>
2023-05-07 18:09:38 -0400

    Update GCDtest.go

--- lecture/GCDtest.go
+++ lecture/GCDtest.go
@@ -23,23 +23,30 @@ func gcd(a int, b int) int {
 			return gcd
 		}
 	}
-	fmt.Println("GCD Between ", a," and ", b," is 1")
+	
 	return 1
 }
 
-// Right now, this code calculates the GCD of 1000 pairs of numbers.
+// Right now, this code calculates the GCD of 250 pairs of numbers.
 // Before starting the exercise, note how long it takes for the code
 // to complete execution.
 
 // The goal of the exercise is to parallelize this code using goroutines.
-// We want to spawn three workers that all take input from a single input channel
+// We want to spawn three goroutines of a gcd function that all take input from a single input channel
 // And send their computed GCD output to a shared output channel.
 
-// This should be done in a way that the amount of workers can change without
+// This should be done in a way that the amount of goroutines can change without
 // anything else needing modification.
 // The format of how the data is printed to the console should also stay the same.
 
-// Hint: The channels may be sending more complex messages than just a number
+// Hint: You will want the channels to input and output information from this struct:
+type gcdmsg struct {
+	a	int
+	b	int
+	out	int
+}
+//Example of a struct declaration: msg := gcdmsg{1,1,0}
+
 
 // As expected, once the workers are correctly implemented, 
 // there should be a significant time saving.
@@ -55,7 +62,8 @@ func main() {
 	for k:=0; k<250;k++{
 		a = rand.Intn(1000000)
 		b = rand.Intn(1000000)
-		gcd(a, b)
+		result:= gcd(a, b)
+		fmt.Println("GCD Between ", a," and ", b," is ,result)
 	}
 	t := time.Now()
 	duration := t.Sub(start)
