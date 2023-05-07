54e35d092129c0635d648a311309f5de2e2db616
rhit-storoeb1 <storoeb1@rose-hulman.edu>
2023-05-07 16:34:33 -0400

    example goroutines code

---
+++ lecture/sum.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func sum(c chan int, co chan int) {
+	for {
+		x := <-c
+		y := <-c
+		fmt.Println(x, "+", y, "=", x+y)
+		co <- x + y
+	}
+}
+
+func main() {
+	c := make(chan int, 6)
+	co := make(chan int, 3)
+	go sum(c, co)
+	c <- 10
+	c <- 15
+	c <- 99
+	c <- 1
+	c <- 23
+	c <- 7
+
+	r := <-co
+	fmt.Println(r)
+	r = <-co
+	fmt.Println(r)
+	r = <-co
+	fmt.Println(r)
+}
