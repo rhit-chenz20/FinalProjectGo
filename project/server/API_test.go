60140177996a07d04448f69c11b04dfdd7bdd4e8
rhit-chenz20 <chenz20@rose-hulman.edu>
2023-05-17 15:53:21 -0400

    unit test cases added

--- project/server/API_test.go
+++ project/server/API_test.go
@@ -1,30 +1,120 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
-func SetUpRouter() *gin.Engine {
+func setupRouter() *gin.Engine {
 	router := gin.Default()
-	router.Run("localhost:8080")
+
+	// router.POST("/students", postStudents)
+
+	// router.GET("/student/:id", getStudentByID)
+
+	// router.POST("/student", postGradeToStudentbyID)
+	// router.GET("/student/:id/course/:course", getStudentsGradeById)
 	return router
 }
 
-func TestHomepageHandler(t *testing.T) {
-	mockResponse := `[ "CSSE403", "CSSE374", "CSSE304" ]`
-	r := SetUpRouter()
-	r.GET("/courses", getCourses)
-	req, _ := http.NewRequest("GET", "/", nil)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
+func TestGetCourses(t *testing.T) {
+	mockResponse := `["CSSE403","CSSE374","CSSE304"]`
+	router := setupRouter()
+	router.GET("/courses", getCourses)
 
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/courses", nil)
+	router.ServeHTTP(w, req)
 	responseData, _ := ioutil.ReadAll(w.Body)
-	assert.Equal(t, mockResponse, string(responseData))
+	response := strings.ReplaceAll(string(responseData), " ", "")
+	response = strings.ReplaceAll(response, "\n", "")
+
+	assert.Equal(t, mockResponse, response)
 	assert.Equal(t, http.StatusOK, w.Code)
 }
+
+func TestPostCourses(t *testing.T) {
+	mockResponse := `"CSSE132"`
+	router := setupRouter()
+	router.POST("/courses", postCourses)
+	router.GET("/courses", getCourses)
+
+	course := "CSSE132"
+	val, _ := json.Marshal(course)
+	req, _ := http.NewRequest("POST", "/courses", bytes.NewBuffer(val))
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	responseData, _ := ioutil.ReadAll(w.Body)
+	response := strings.ReplaceAll(string(responseData), " ", "")
+	response = strings.ReplaceAll(response, "\n", "")
+
+	assert.Equal(t, mockResponse, response)
+	assert.Equal(t, http.StatusCreated, w.Code)
+
+	mockResponse = `["CSSE403","CSSE374","CSSE304","CSSE132"]`
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/courses", nil)
+	router.ServeHTTP(w, req)
+	responseData, _ = ioutil.ReadAll(w.Body)
+	response = strings.ReplaceAll(string(responseData), " ", "")
+	response = strings.ReplaceAll(response, "\n", "")
+
+	assert.Equal(t, mockResponse, response)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestGetStudent(t *testing.T) {
+	mockResponse := `[{"ID":"1","Name":"JohnDoe","Year":"Junior","Courses":{}},{"ID":"2","Name":"JaneDoe","Year":"Sophomore","Courses":{}},{"ID":"3","Name":"DaveSmith","Year":"Senior","Courses":{}}]`
+	router := setupRouter()
+	router.GET("/students", getStudents)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/students", nil)
+	router.ServeHTTP(w, req)
+	responseData, _ := ioutil.ReadAll(w.Body)
+	response := strings.ReplaceAll(string(responseData), " ", "")
+	response = strings.ReplaceAll(response, "\n", "")
+
+	assert.Equal(t, mockResponse, response)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+// func TestPostStudent(t *testing.T) {
+// 	mockResponse := `"CSSE132"`
+// 	router := setupRouter()
+// 	router.POST("/courses", postCourses)
+// 	router.GET("/courses", getCourses)
+
+// 	course := "CSSE132"
+// 	val, _ := json.Marshal(course)
+// 	req, _ := http.NewRequest("POST", "/courses", bytes.NewBuffer(val))
+// 	w := httptest.NewRecorder()
+// 	router.ServeHTTP(w, req)
+// 	responseData, _ := ioutil.ReadAll(w.Body)
+// 	response := strings.ReplaceAll(string(responseData), " ", "")
+// 	response = strings.ReplaceAll(response, "\n", "")
+
+// 	assert.Equal(t, mockResponse, response)
+// 	assert.Equal(t, http.StatusCreated, w.Code)
+
+// 	mockResponse = `["CSSE403","CSSE374","CSSE304","CSSE132"]`
+
+// 	w = httptest.NewRecorder()
+// 	req, _ = http.NewRequest("GET", "/courses", nil)
+// 	router.ServeHTTP(w, req)
+// 	responseData, _ = ioutil.ReadAll(w.Body)
+// 	response = strings.ReplaceAll(string(responseData), " ", "")
+// 	response = strings.ReplaceAll(response, "\n", "")
+
+// 	assert.Equal(t, mockResponse, response)
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// }
